758c4723b10e6660147bea522f7e95036b158e58
Sai Varma <varmarayapati2002@gmail.com>
2026-06-12 12:40:27 +0530

    config maps & secrets added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const host = process.env.MONGO_HOST;
+const port = process.env.MONGO_PORT;
+const dbname = process.env.MONGO_DBNAME;
+
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+
+const mongoURI =
+  `mongodb://${user}:${password}@${host}:${port}/${dbname}?authSource=admin`;
+
+
 const todoRoutes = require("./routes/todoRoutes");
 
 const authRoutes = require("./routes/authRoutes");
@@ -16,11 +27,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch(err => console.log(err));
+mongoose.connect(mongoURI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
     res.send("Backend Running");
