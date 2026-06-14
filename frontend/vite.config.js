156248e71234a4d9a326e1f20cfa7b0d4f29e831
varmasa <97676986+varmasa@users.noreply.github.com>
2026-06-14 23:19:19 +0530

    Update for ingress

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+
+    allowedHosts: true,
+
+    hmr: {
+      protocol: "ws",
+      host: "todo.local",
+      clientPort: 80
+    }
+  }
 })
